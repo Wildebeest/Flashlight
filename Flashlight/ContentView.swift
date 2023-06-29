f0dcf02595fde57999586a167b32993b44a156a7
Matthew Wilde <matthewcwilde@gmail.com>
2023-06-29 15:04:29 -0400

    Initial Commit

---
+++ Flashlight/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Flashlight
+//
+//  Created by Matt Wilde on 6/22/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "old_lightsaber_tree", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashlight/FlashlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashlightApp.swift
+//  Flashlight
+//
+//  Created by Matt Wilde on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
